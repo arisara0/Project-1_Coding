5f470404a3fae46c00478447ffa6cd83950fe9ec
arisara0 <xrisraswangci22@gmail.com>
2024-12-04 16:07:36 +0700

    Add files via upload

---
+++ 1form_37mook.py
@@ -0,0 +1,10 @@
+print("welcome to 4/4")
+
+print("\n\nสวัสดีค่ะ กรุณากรอกข้อมูลค่ะ")
+nickname = input("ชื่อเล่น")
+id_student = input("รหัสนักเรียน")
+service = input("ยืมปากกา")
+
+print("\n\nยินดีต้อนรับค่ะ", nickname)
+print("รหัสนักเรียน", id_student , "\กำลังใช้บริการ" ,service)
+print("\n\nขอบคุณที่ใช้บริการค่ะ")
